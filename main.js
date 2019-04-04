2562672fe4aa4e913cbb510be3e107d83227187d
Damyako <44761678+Damyako@users.noreply.github.com>
2019-04-04 03:04:29 -0400

    Mobile frontend

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+	if(window.innerWidth <= 600) {
+		var dNav = document.querySelector("#detailsNav");
+		var circles = dNav.querySelectorAll("span");
+		var humidDetails = document.querySelector("#humid");
+		var sunDetails = document.querySelector("#sun");
+		var tempDetails = document.querySelector("#temp");
+		var current;
 
-var mainWindow = null;
+		if(window.location.href.indexOf("temp") > -1) {
+			tempDetails.classList.remove("hideDetailsRight");
+			tempDetails.classList.remove("hideDetailsLeft");
+			humidDetails.classList.add("hideDetailsLeft");
+			circles[1].classList.add("active");
+			circles[0].classList.remove("active");
+			current = circles[1];
+		} else if(window.location.href.indexOf("sun") > -1) {
+			humidDetails.classList.add("hideDetailsLeft");
+			sunDetails.classList.remove("hideDetailsRight");
+			sunDetails.classList.remove("hideDetailsLeft");
+			circles[2].classList.add("active");
+			circles[0].classList.remove("active");
+			current = circles[2];
+		} else {
+			current = circles[0];
+		}
 
-app.on('ready', function () {
-  mainWindow = new BrowserWindow({
-    height: 505,
-    resizable: true,
-    title: 'j5/electron template',
-    width: 595,
-	frame: false
-  });
-
-  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-});
+		for (var h = 0; h < circles.length; h++) {
+			circles[h].addEventListener("click", swipeNav, false);
+		};
+	}
